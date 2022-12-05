36b64f0c4d49b96aefe2ab725e9fc7e882a419eb
fdz15 <84093768+picole77@users.noreply.github.com>
2022-12-05 06:54:48 -0600

    add crud

--- app/src/main/java/edu/itvo/quotes/QuotesApp.kt
+++
@@ -1,7 +0,0 @@
-package edu.itvo.quotes
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class QuotesApp: Application()
--- app/src/main/java/edu/itvo/quotes/domain/model/QuoteModel.kt
+++
@@ -1,4 +0,0 @@
-package edu.itvo.quotes.domain.model
-
-data class QuoteModel(val id:Int,val quote: String, val author: String) {
-}
